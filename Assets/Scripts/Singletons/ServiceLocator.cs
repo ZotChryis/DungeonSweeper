7404c1fdea943e54daaa111fca84d5648927e9fa
Christian Nava <navac@outlook.com>
2025-08-11 15:11:31 -0700

    Updates needed for Android issues

--- Assets/Scripts/Singletons/ServiceLocator.cs
+++ Assets/Scripts/Singletons/ServiceLocator.cs
@@ -57,6 +57,8 @@ public class ServiceLocator : SingletonMonoBehaviour<ServiceLocator>
         {
             DontDestroyOnLoad(gameObject);
 
+            Application.targetFrameRate = 60;
+            
             Schemas = new SchemaContainer();
             Schemas.Initialize();
 
