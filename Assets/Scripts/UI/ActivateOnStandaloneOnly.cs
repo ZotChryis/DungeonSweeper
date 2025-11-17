5e3356f6467bd3661d44810c066130eb2ed42eeb
gw1108 <georgetw1108@gmail.com>
2025-11-16 19:51:19 -0800

    Added goldbug enemy. Removed gnome from level 1 tutorial version. Made cheats only available for standalone client plus android plus iOS

---
+++ Assets/Scripts/UI/ActivateOnStandaloneOnly.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class ActivateOnStandaloneOnly : MonoBehaviour
+{
+    private void Start()
+    {
+#if (UNITY_STANDALONE_WIN || UNITY_STANDALONE_LINUX || UNITY_STANDALONE_OSX || STEAMWORKS_WIN || STEAMWORKS_LIN_OSX || UNITY_IOS || UNITY_ANDROID)
+        // Only show this gameobject for standalone platform (Mac, Windows or Linux) or Android or IOS.
+        gameObject.SetActive(true);
+#else
+        gameObject.SetActive(false);
+#endif
+    }
+}
