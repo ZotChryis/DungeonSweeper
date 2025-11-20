2fa3d5c133eff740faeb60147b278b3e555a6dd1
gw1108 <georgetw1108@gmail.com>
2025-11-20 15:24:06 -0800

    Added steam upsell next to discord on main menu.

---
+++ Assets/Scripts/Steamworks.NET/OpenSteam.cs
@@ -0,0 +1,27 @@
+﻿
+using Steamworks;
+using UnityEngine;
+
+public class OpenSteam : MonoBehaviour
+{
+    private const uint DungeonSweeperAppId = 4109840;
+
+    public void OpenSteamPage()
+    {
+        if (SteamManager.Initialized)
+        {
+            try
+            {
+                SteamFriends.ActivateGameOverlayToStore(new AppId_t(DungeonSweeperAppId), EOverlayToStoreFlag.k_EOverlayToStoreFlag_None);
+            }
+            catch
+            {
+                Application.OpenURL("steam://store/4109840");
+            }
+        }
+        else
+        {
+            Application.OpenURL("steam://store/4109840");
+        }
+    }
+}
