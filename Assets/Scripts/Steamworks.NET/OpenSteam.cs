30c3c52d8e8347baceb511c09e6a3a7f1e2feb70
gw1108 <georgetw1108@gmail.com>
2025-11-20 14:59:26 -0800

    Update steam wishlist call to action to only open the overlay. If that fails then we try to open the url.

--- Assets/Scripts/Steamworks.NET/OpenSteam.cs
+++ Assets/Scripts/Steamworks.NET/OpenSteam.cs
@@ -1,10 +1,23 @@
 
+using Steamworks;
 using UnityEngine;
 
 public class OpenSteam : MonoBehaviour
 {
+    private const uint DungeonSweeperAppId = 4109840;
+
     public void OpenSteamPage()
     {
-        Application.OpenURL("steam://store/4109840");
+        if (SteamManager.Initialized)
+        {
+            try
+            {
+                SteamFriends.ActivateGameOverlayToStore(new AppId_t(DungeonSweeperAppId), EOverlayToStoreFlag.k_EOverlayToStoreFlag_None);
+            }
+            catch
+            {
+                Application.OpenURL("steam://store/4109840");
+            }
+        }
     }
 }
